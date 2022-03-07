682b68036d4fec69d7faa7d3233884fc373fb197
Jason Bell <jbell@accelebrate.com>
2022-03-07 09:28:47 -0800

    Initial EComm

---
+++ Demos/LangDemo/LangDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace LangDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person() {
+                FirstName = "Bill",
+                LastName = "Gates"
+            };
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Person
+    {
+        public string FirstName { get; init; } = string.Empty;
+        public string LastName { get; init; } = string.Empty;
+    }
+}
