03afef5032490e99b8fd48b35fd0f930052e9e3c
Jason Bell <jbell@accelebrate.com>
2022-03-08 12:56:24 -0800

    Sync

--- EComm/EComm/Server/Controllers/ProductController.cs
+++ EComm/EComm/Server/Controllers/ProductController.cs
@@ -7,7 +7,6 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace EComm.Server.Controller
@@ -26,16 +25,26 @@ namespace EComm.Server.Controller
         [HttpGet("")]
         public async Task<IActionResult> GetAllProducts()
         {
-            var products = _dataContext.Products;
+            var products = await _dataContext.Products.ToArrayAsync();
+            return Ok(products);
+        }
 
-            var sp = products.Where(p => p.ProductName.StartsWith("A"));
-
-            var sp2 = sp.Where(p => p.UnitPrice > 5);
-
-
-            var retVal = await sp2.ToArrayAsync();
-            
-            return Ok(retVal);
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = await _dataContext.Products.SingleOrDefaultAsync(p => p.Id == id);
+            if (product == null) return NotFound();
+            return Ok(product);
         }
     }
 }
+
+// Rest of the day....
+
+//  Get all the products
+//  Get one product based on id
+//  Add a new product
+//  Replace an existing product
+//  Delete a product
+
+//  BONUS: Modify part of a product
