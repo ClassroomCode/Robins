00a93aec8e98a0d580ad0226bd3e340732c22c22
Jason Bell <jbell@accelebrate.com>
2022-03-08 07:30:33 -0800

    Models

---
+++ EComm/EComm/Shared/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EComm.Shared
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public int SupplierId { get; set; }
+        public Decimal? UnitPrice { get; set; }
+        public string? Package { get; set; }
+        public bool IsDiscontinued { get; set; }
+
+        public Supplier? Supplier { get; set; }
+    }
+}
