da580001e5fbb51e53140aab9a5ee5655f5fa564
Jason Bell <jbell@accelebrate.com>
2022-03-08 08:00:55 -0800

    DbContext

---
+++ EComm/EComm/Server/DataAccess/ECommDataContext.cs
@@ -0,0 +1,15 @@
+﻿using EComm.Shared;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EComm.Server.DataAccess
+{
+    public class ECommDataContext : DbContext
+    {
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<Supplier> Suppliers => Set<Supplier>();
+    }
+}
