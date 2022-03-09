dc1fde20b5c88cd096870f598927e746e815f5fa
Jason Bell <jbell@accelebrate.com>
2022-03-09 11:31:43 -0800

    Save

--- EComm/EComm/Shared/Product.cs
+++ EComm/EComm/Shared/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace EComm.Shared
     public class Product
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Product must have a name")]
         public string ProductName { get; set; } = string.Empty;
         public int SupplierId { get; set; }
         public Decimal? UnitPrice { get; set; }
