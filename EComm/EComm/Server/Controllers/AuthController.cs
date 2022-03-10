2aadc11df09572ff084940c38e7b4277c38f6ff1
Jason Bell <jbell@accelebrate.com>
2022-03-10 11:40:19 -0800

    Auth fixed

--- EComm/EComm/Server/Controllers/AuthController.cs
+++ EComm/EComm/Server/Controllers/AuthController.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using EComm.Shared;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace EComm.Server.Controllers
@@ -11,5 +16,35 @@ namespace EComm.Server.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto request)
+        {
+            if (request.Username == "test" && request.Password == "password") {
+                return Ok($"{request.Username}|admin");
+            }
+            else {
+                return Unauthorized();
+            }
+
+            /*
+            var key = Encoding.UTF8.GetBytes(_configuration["key"]);
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(5),
+                signingCredentials: creds);
+
+            return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+            */
+        }
     }
 }
