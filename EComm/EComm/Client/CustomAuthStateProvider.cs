7d9bc08520183dcc51b626f3f52800fbec7814b5
Jason Bell <jbell@accelebrate.com>
2022-03-10 10:49:50 -0800

    A3

--- EComm/EComm/Client/CustomAuthStateProvider.cs
+++ EComm/EComm/Client/CustomAuthStateProvider.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,17 +10,29 @@ namespace EComm.Client
 {
     public class CustomAuthStateProvider : AuthenticationStateProvider
     {
+        private ILocalStorageService _localStorage;
+
+        public CustomAuthStateProvider(ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+        }
+
         public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-            //var identity = new ClaimsIdentity();
-            
-            var identity = new ClaimsIdentity(new[]
-            {
-                new Claim(ClaimTypes.Name, "Bill Gates"),
-                new Claim(ClaimTypes.Role, "Admin")
-            }, "My Custom Auth");
-            
+            string token = await _localStorage.GetItemAsStringAsync("token");
 
+            ClaimsIdentity identity = new ClaimsIdentity();
+
+            if (!string.IsNullOrEmpty(token)) {
+                // 
+
+                identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, "Bill Gates"),
+                    new Claim(ClaimTypes.Role, "Admin")
+                }, "My Custom Auth");
+            }
+            
             var principal = new ClaimsPrincipal(identity);
 
             var state = new AuthenticationState(principal);
