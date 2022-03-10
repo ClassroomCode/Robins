5291d8d095532035befd897df433bc55c4fb42b2
Jason Bell <jbell@accelebrate.com>
2022-03-10 09:38:49 -0800

    CAP

---
+++ EComm/EComm/Client/CustomAuthStateProvider.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace EComm.Client
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var identity = new ClaimsIdentity();
+            /*
+            var identity = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Name, "Bill Gates"),
+                new Claim(ClaimTypes.Role, "Admin")
+            }, "My Custom Auth");
+            */
+
+            var principal = new ClaimsPrincipal(identity);
+
+            var state = new AuthenticationState(principal);
+
+            NotifyAuthenticationStateChanged(Task.FromResult(state));
+
+            return state;
+        }
+    }
+}
