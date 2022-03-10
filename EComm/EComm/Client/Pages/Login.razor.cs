3b615d08697f4897fb2c3ab7b57ea3bd69d2d8f7
Jason Bell <jbell@accelebrate.com>
2022-03-10 12:12:09 -0800

    Auth

---
+++ EComm/EComm/Client/Pages/Login.razor.cs
@@ -0,0 +1,22 @@
+﻿using EComm.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace EComm.Client.Pages
+{
+    public partial class Login
+    {
+        private LoginDto user = new LoginDto();
+
+        async Task HandleLogin()
+        {
+            var result = await Http.PostAsJsonAsync("api/auth/login", user);
+            var token = await result.Content.ReadAsStringAsync();
+            await LocalStorage.SetItemAsync("token", token);
+            await AuthStateProvider.GetAuthenticationStateAsync();
+        }
+    }
+}
