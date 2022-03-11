6444ca89f1f5527d270e000fafb0e6d18c6f7d47
Jason Bell <jbell@accelebrate.com>
2022-03-11 10:01:37 -0800

    PWA

---
+++ EComm/EComm/Server/Controllers/HealthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EComm.Server.Controllers
+{
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("ping")]
+        public IActionResult Ping()
+        {
+            return Content("Hello");
+        }
+    }
+}
