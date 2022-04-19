b348f33ae236f8987ff9ba271e44271c83f68fc7
Jason Bell <jbell@accelebrate.com>
2022-04-19 09:28:54 -0700

    EComm1

---
+++ Demos/CSharpFeatures/CSharpFeatures/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CSharpFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new();
+            p.FirstName = "Bill";
+
+            string str1 = "Hello";
+            string str2 = "Hello";
+            string str3 = "Hello";
+
+            Person p2 = p;
+
+            if (p == p2) {
+                //
+            }
+
+            string str = "Test";
+            str = str.ToUpper();
+
+            DateTime dt = DateTime.Now;
+            dt.AddDays(3);
+
+
+            Console.WriteLine(p.FirstName);
+        }
+    }
+
+    struct Person
+    {
+        public string FirstName;
+        public string LastName;
+    }
+}
+
