93fa3eb2834a8f017a841fd38835d4e7d28e8a4c
Jason Bell <jbell@accelebrate.com>
2022-04-20 06:00:13 -0700

    Interfaces

---
+++ EComm/EComm.Abstractions/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using EComm.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EComm.Abstractions
+{
+    public interface IRepository
+    {
+        Task<IEnumerable<Product>> GetAllProducts();
+        Task<IEnumerable<Supplier>> GetAllSuppliers();
+    }
+}
---
+++ EComm/EComm.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EComm.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
