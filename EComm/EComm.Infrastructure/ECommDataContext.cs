06e672eb707a7d24172de346c042dedc6fa9d2d9
Jason Bell <jbell@accelebrate.com>
2022-04-20 06:40:21 -0700

    DbContext

---
+++ EComm/EComm.Infrastructure/ECommDataContext.cs
@@ -0,0 +1,27 @@
+﻿using EComm.Abstractions;
+using EComm.Shared;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EComm.Infrastructure
+{
+    public class ECommDataContext : DbContext, IRepository
+    {
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<Supplier> Suppliers => Set<Supplier>();
+
+        public async Task<IEnumerable<Product>> GetAllProducts()
+        {
+            return await Products.ToArrayAsync();
+        }
+
+        public Task<IEnumerable<Supplier>> GetAllSuppliers()
+        {
+            return await Suppliers.ToArrayAsync();
+        }
+    }
+}
