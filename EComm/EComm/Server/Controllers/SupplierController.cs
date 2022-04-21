6285113ca9a9591a0dbc547573ee2a0bf354adaf
Jason Bell <jbell@accelebrate.com>
2022-04-21 12:10:06 -0700

    Edit p4

---
+++ EComm/EComm/Server/Controllers/SupplierController.cs
@@ -0,0 +1,34 @@
+﻿using EComm.Abstractions;
+using EComm.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EComm.Server.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SupplierController : ControllerBase
+    {
+        private IRepository _repository;
+
+        public SupplierController(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("")]  
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAllSuppliers()
+        {
+            var suppliers = await _repository.GetAllSuppliers();
+
+            return Ok(suppliers);
+        }
+    }
+}
+
--- EComm/EComm/Shared/Product.cs
+++ EComm/EComm/Shared/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,15 @@ namespace EComm.Shared
     public class Product
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="Product must have a name")]
         public string ProductName { get; set; } = string.Empty;
+
         public int SupplierId { get; set; }
+
+        [Required, Range(1.0, 500.0)]
         public Decimal? UnitPrice { get; set; }
+
         public string? Package { get; set; }
         public bool IsDiscontinued { get; set; }
 
