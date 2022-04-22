00b6546573d32536e560345243dac6ccac71a43d
Jason Bell <jbell@accelebrate.com>
2022-04-22 11:04:14 -0700

    Tests

---
+++ EComm/EComm.Tests/CounterTest.cs
@@ -0,0 +1,24 @@
+﻿using Bunit;
+using EComm.Client.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EComm.Tests
+{
+    public class CounterTest : TestContext
+    {
+        [Fact]
+        public void SimpleCounterTest()
+        {
+            //Services.AddScoped<ILocalStorageService, StubLocalStorageService>();
+
+            var c = RenderComponent<FetchData>();
+
+            Assert.Equal("", c.Markup);
+        }
+    }
+}
