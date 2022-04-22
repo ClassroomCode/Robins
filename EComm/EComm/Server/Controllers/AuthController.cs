14d061a5bcd5ce83ed882ef49d9f46205f70b6c5
Jason Bell <jbell@accelebrate.com>
2022-04-22 07:20:29 -0700

    Auth p2

---
+++ EComm/EComm/Server/Controllers/AuthController.cs
@@ -0,0 +1,51 @@
+﻿using EComm.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EComm.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [Route("login")]
+        public IActionResult Login(LoginDto request)
+        {
+            var isAuth = (request.Username == "test" && request.Password == "password");
+
+            if (!isAuth) return Unauthorized();
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var claims = new[] {
+                new Claim(ClaimTypes.NameIdentifier, "someuser"),
+                new Claim(ClaimTypes.Name, "Bill Gates"),
+                new Claim(ClaimTypes.Role, "Admin")
+            };
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(5),
+                signingCredentials: creds);
+
+            return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+        }
+    }
+}
