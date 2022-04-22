09d25553e5694d0f571f1aaa5c7f209056a79071
Jason Bell <jbell@accelebrate.com>
2022-04-22 08:01:46 -0700

    Auth p3

---
+++ EComm/EComm/Client/CustomAuthStateProvider.cs
@@ -0,0 +1,65 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace EComm.Client
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        private ILocalStorageService _localStorage;
+        private HttpClient _httpClient;
+
+        public CustomAuthStateProvider(ILocalStorageService localStorage, HttpClient httpClient)
+        {
+            _localStorage = localStorage;
+            _httpClient = httpClient;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            string token = await _localStorage.GetItemAsStringAsync("token");
+
+            ClaimsIdentity identity = new ClaimsIdentity();
+
+            if (!string.IsNullOrEmpty(token)) {
+                token = token.Replace("\"", "");
+                identity = new ClaimsIdentity(ParseClaimsFromJwt(token), "My Custom Auth");
+            }
+            var principal = new ClaimsPrincipal(identity);
+            var state = new AuthenticationState(principal);
+
+            _httpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", token);
+
+            NotifyAuthenticationStateChanged(Task.FromResult(state));
+
+            return state;
+        }
+
+        public static IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+        {
+            var payload = jwt.Split('.')[1];
+            var jsonBytes = ParseBase64WithoutPadding(payload);
+            var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+            if (keyValuePairs == null) return new List<Claim>();
+
+            return keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString() ?? ""));
+        }
+
+        private static byte[] ParseBase64WithoutPadding(string base64)
+        {
+            switch (base64.Length % 4) {
+                case 2: base64 += "=="; break;
+                case 3: base64 += "="; break;
+            }
+            return Convert.FromBase64String(base64);
+        }
+    }
+}
--- EComm/EComm/Client/Program.cs
+++ EComm/EComm/Client/Program.cs
@@ -1,3 +1,5 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +20,9 @@ namespace EComm.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddBlazoredLocalStorage();
+            builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+            builder.Services.AddAuthorizationCore();
 
             await builder.Build().RunAsync();
         }
